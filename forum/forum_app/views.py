526979ff5f60ad2f18c71ce0f423878cd965e12d
Brandon Benton <brandon.benton.5255@gmail.com>
2025-05-29 05:26:03 -0500

    fixing posts, replies

--- forum/forum_app/views.py
+++ forum/forum_app/views.py
@@ -1,35 +1,36 @@
-from django.shortcuts import render, redirect
-from .models import *
-from .forms import *
-
+from django.shortcuts import render,redirect
+from .models import * 
+from .forms import * 
+# Create your views here.
+ 
 def home(request):
-    forums = forum.objects.all()
-    count = forums.count()
-    discussions = []
+    forums=forum.objects.all()
+    count=forums.count()
+    discussions=[]
     for i in forums:
         discussions.append(i.discussion_set.all())
-    
-    context = {'forums':forums,
-                'count':count,
-                'discusisons':discussions}
+ 
+    context={'forums':forums,
+              'count':count,
+              'discussions':discussions}
     return render(request,'home.html',context)
-
+ 
 def addInForum(request):
     form = CreateInForum()
     if request.method == 'POST':
-        forum = CreateInForum(request.POST)
-        if form.is_valid():
-            forum.save()
-            return redirect('/')
-        context = {'form':form}
-        return render(request,'addInForum.html',context)
-
-def addInDiscussion(request):
-    form = CreateInDiscussion()
-    if request.method == 'POST':
-        forum = CreateInDiscussion(request.POST)
+        form = CreateInForum(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/')
-    context = {'form':form}
+    context ={'form':form}
+    return render(request,'addInForum.html',context)
+ 
+def addInDiscussion(request):
+    form = CreateInDiscussion()
+    if request.method == 'POST':
+        form = CreateInDiscussion(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context ={'form':form}
     return render(request,'addInDiscussion.html',context)
