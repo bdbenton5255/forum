8cf8f4b88eb9045e950fef7df416db8935412e8f
Brandon Benton <brandon.benton.5255@gmail.com>
2025-05-28 21:06:51 -0500

    urls

--- forum/forum/urls.py
+++ forum/forum/urls.py
@@ -19,4 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home,name='home'),
+    path('addInForum/',addInForum, name='addInForum'),
+    path('addInDiscussion/',addInDiscussion,name='addInDiscussion'),
 ]
